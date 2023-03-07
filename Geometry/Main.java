7e876cdaab11990490e2faa819411628991e924d
Jatteliten <daniel.isaksson90@gmail.com>
2023-03-07 14:55:04 +0100

    Add files via upload

---
+++ Geometry/Main.java
@@ -0,0 +1,26 @@
+package Geometry;
+import java.awt.*;
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+    public Main(){
+
+    }
+
+    public static void main(String[] args){
+
+
+        Panel p = new Panel();
+        p.setBackground(Color.LIGHT_GRAY);
+
+        JFrame db = new JFrame("Geometry");
+        db.setSize(450,300);
+        db.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        db.add(p);
+        db.setVisible(true);
+        
+    }
+
+
+}
